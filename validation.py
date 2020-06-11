faa2b961d181315a98bab593d9f06ea533835bf5
Dhriti Garg <58422781+dhritigarg02@users.noreply.github.com>
2020-06-11 18:23:58 +0530

    Add files via upload

---
+++ validation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from Crypto.Signature import pss
+from hashlib import sha256
+
+def ValidateTxn(Txn, unused_outputs):
+
+    flag = True
+    total_input_coins = 0
+    total_output_coins = 0
+    temp_used_outputs = {}
+
+    for Input in Txn.inputs:
+        try:
+            total_input_coins += unused_outputs[Input.transID][Input.index]["coins"]
+        except:
+            flag = False
+            return flag
+    for Output in Txn.outputs:
+        total_output_coins += int.from_bytes(Output.amount, 'big')
+
+    if total_output_coins > total_input_coins:
+        flag = False
+        return flag
+
+    for Input in Txn.inputs:
+        try:
+            verifier = pss.new(unused_outputs[Input.transID][Input.index]["Publickey"])
+            data = Input.transID + Input.index.to_bytes(4, 'big') + Txn.num_outputs.to_bytes(4, 'big') + sha256(Txn.output_bytes).digest()
+            verifier.verify(data, Input.signature)
+
+            temp_used_outputs[Input.transID][Input.index] = unused_outputs[Input.transID].pop[Input.index]
+        except:
+            for transId in temp_used_outputs:
+                if transID in unused_outputs:
+                    unused_outputs.update(temp_used_outputs[transID])
+                else:
+                    unused_outputs[transID] = temp_used_outputs[transID]
+
+            flag = False
+            return flag
+
+
